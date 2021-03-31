a53526f2d846d08db41d78faa11ba89ec9f3e581
Sergei3190 <sergkosya@mail.ru>
2021-03-31 17:05:46 +0300

    добавлены вспомогательные классы для функционирования приложени

---
+++ SqlBulkCopyVSMergeAsync/Helpers/SourceTestDataBuilder.cs
@@ -0,0 +1,85 @@
+﻿using NLog;
+using SqlBulkCopyVSMergeAsync.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+
+namespace SqlBulkCopyVSMergeAsync.Helpers
+{
+    public class SourceTestDataBuilder
+    {
+        private readonly Logger _log;
+        private readonly Random _random;
+
+        public SourceTestDataBuilder()
+        {
+            _log = LogManager.GetLogger(nameof(SourceTestDataBuilder));
+            _random = new Random();
+        }
+
+        public List<int> GetPoints()
+        {
+            var points = new List<int>();
+
+            for (int i = 1; i <= 100_000; i++)
+            {
+                points.Add(_random.Next(1, 100_001));
+            }
+
+            return points.Distinct<int>().ToList();
+        }
+
+        public List<long> GetRequestsId()
+        {
+            var requetsId = new List<long>();
+
+            try
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    requetsId.Add(Convert.ToInt64(DateTime.Now.AddMinutes(i).ToString("yMdHms", CultureInfo.InvariantCulture)));
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error($"Ошибка при выполнении метода GetRequestsId\r\n{ex}");
+                new List<long>();
+            }
+
+            return requetsId;
+        }
+
+        public Dictionary<string, DataTable> GetCompletedClientTables(in List<int> points, in List<long> requestsId)
+        {
+            ClientTablesBuilder builder = new ClientTablesBuilder();
+
+            var clientTables = builder.GetEmptyClientTables();
+
+            foreach (var item in clientTables)
+            {
+                var result = builder.FillClientTables(item.Value, points, requestsId);
+
+                if (result != 1)
+                {
+                    _log.Info($"Данных для заполнения таблицы массового копирования нет");
+                    return null;
+                }
+            }
+
+            return clientTables;
+        }
+
+        public Dictionary<string, string> GetTablesNamesDB()
+        {
+            return new Dictionary<string, string>()
+            {
+                {"PointsToRetryMergePK", "AddValuesPointsToRetryPK"},
+                {"PointsToRetryMergeFK", "AddValuesPointsToRetryFK"},
+                {"PointsToRetrySqlBulkCopyPK", "ExecuteBulkImportPointsToRetry" },
+                {"PointsToRetrySqlBulkCopyFK", "ExecuteBulkImportPointsToRetry" }
+            };
+        }
+    }
+}
---
+++ SqlBulkCopyVSMergeAsync/Helpers/SourceTestDataBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace SqlBulkCopyVSMergeAsync.Helpers
+{
+    public static class SourceTestDataBuilderExtensions
+    {
+        private static readonly Logger _log = LogManager.GetLogger(nameof(SourceTestDataBuilderExtensions));
+
+        public static async Task<(List<int> points, List<long> requestsId, Dictionary<string, string> tablesNamesDB)> GetObjectsToFillAsync(this SourceTestDataBuilder builder)
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            var points = Task.Run(() => builder.GetPoints());
+            var requestsId = Task.Run(() => builder.GetRequestsId());
+            var tableNamesDB = Task.Run(() => builder.GetTablesNamesDB());
+
+            var objectsToFill = Task.WhenAll(points, requestsId, tableNamesDB);
+            await objectsToFill;
+
+            return (points.Result, requestsId.Result, tableNamesDB.Result);
+        }
+
+        public static async Task<Dictionary<string, DataTable>> GetFilledObjectsAsync(this SourceTestDataBuilder builder, List<int> points, List<long> requestsId)
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (points is null)
+                throw new ArgumentNullException(nameof(points));
+
+            if (requestsId is null)
+                throw new ArgumentNullException(nameof(requestsId));
+
+            return await Task.Run(() => builder.GetCompletedClientTables(points, requestsId)).ConfigureAwait(false);
+        }
+    }
+}
