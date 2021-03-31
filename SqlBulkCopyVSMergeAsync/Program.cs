653d1fceb218225979e0f3443fab5b982c54226d
Sergei3190 <sergkosya@mail.ru>
2021-03-31 17:04:57 +0300

    добавлен каркас и настройки приложения SqlBulkCopyVSMergeAsync

---
+++ SqlBulkCopyVSMergeAsync/Program.cs
@@ -0,0 +1,15 @@
+﻿using SqlBulkCopyVSMergeAsync.Handlers;
+using System.Threading.Tasks;
+
+namespace SqlBulkCopyVSMergeAsync
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Worker worker = new Worker();
+            return await worker.RunWorkerAsync().ConfigureAwait(false);
+        }
+    }
+}
+
